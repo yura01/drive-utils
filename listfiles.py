997e7bab69253bee7152de5ef0db9356966e769d
Yuri Kyrpach <ykyrpach@google.com>
2017-05-31 15:25:29 -0400

    refactor to a class, some tests

--- listfiles.py
+++ listfiles.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
-import os
 
+import os
 import httplib2
 from apiclient import discovery
+import argparse
 import logging
 
 import creds
+
+flags = argparse.ArgumentParser().parse_args()
 
 # time to stop
 _MAX_COUNT = 10000
@@ -15,47 +18,13 @@ _PAGE_SIZE = 500;
 
 account_name = 'quickstart'
 
-_LOG_FILENAME='listfiles.log'
+_LOG_FILENAME = 'listfiles.log'
 _LOG_FILE_PATH = 'log'
 
 if not os.path.exists(_LOG_FILE_PATH):
     os.makedirs(_LOG_FILE_PATH)
 
 logging.basicConfig(filename='%s/%s' % (_LOG_FILE_PATH, _LOG_FILENAME), level=logging.DEBUG)
-
-def get_service():
-    credentials = creds.get_credentials(account_name)
-    http = credentials.authorize(httplib2.Http())
-    return discovery.build('drive', 'v3', http=http)
-
-
-def read_chunk(service, continuation_token):
-    return service.files().list(
-        pageSize=_PAGE_SIZE, pageToken=continuation_token,
-        includeTeamDriveItems=False,
-        corpora="user",
-        spaces="drive",
-        fields="nextPageToken, files(id, name, size, md5Checksum, mimeType, fullFileExtension, originalFilename,"
-               "description, modifiedTime, trashed, parents)"
-        ).execute()
-
-
-def getItem(id):
-    """Finds item by fileId and returns it's name and parent's fileId."""
-    return service.files().get(fileId=id, fields='parents,name').execute()
-
-
-#TODO: @memoize
-def getPath(id):
-    """Builds path of the containing folder by the folder's fileId."""
-    parents = []
-    folder = getItem(id)
-    while folder.get('parents') != [id]:
-        parents.insert(0, folder.get('name'))
-        parentId = folder.get('parents')[0]
-        folder = getItem(parentId)
-    return '/'.join(parents)
-
 
 class ItemKey(object):
     def __init__(self, item):
@@ -65,10 +34,116 @@ class ItemKey(object):
         self.mimeType = item.get('mimeType')
         self.originalFilename = item.get('originalFilename')
 
+    # def __str__(self):
+    #     return str(self.__dict__)
 
-def makeKey(item):
-    return ItemKey(item)
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
 
+class ListFiles(object):
+
+    def __init__(self, service=None):
+        """Service: use None for actual drive service, or pass a mock."""
+        self._service  = service
+        if service == None:
+            self._service = self.get_service()
+
+    def connectToDrive(self):
+        credentials = creds.get_credentials(account_name)
+        http = credentials.authorize(httplib2.Http())
+        return discovery.build('drive', 'v3', http=http)
+
+
+    def readChunk(self, service, continuation_token):
+        return service.files().list(
+            pageSize=_PAGE_SIZE, pageToken=continuation_token,
+            includeTeamDriveItems=False,
+            corpora="user",
+            spaces="drive",
+            fields="nextPageToken, files(id, name, size, md5Checksum, mimeType, fullFileExtension, originalFilename,"
+                   "description, modifiedTime, trashed, parents)"
+        ).execute()
+
+
+    def getItem(self, id):
+        """Finds item by fileId and returns it's name and parent's fileId."""
+        return self._service.files().get(fileId=id, fields='parents,name').execute()
+
+
+    # TODO: @memoize
+    def getPath(self, id):
+        """Builds path of the containing folder by the folder's fileId."""
+        parents = []
+        folder = self.getItem(id)
+        while folder.get('parents') != [id]:
+            parents.insert(0, folder.get('name'))
+            parentId = folder.get('parents')[0]
+            folder = self.getItem(parentId)
+        return '/'.join(parents)
+
+    def loadDocs(self):
+        service = self.connectToDrive()
+        count = 0
+        no_key_count = 0
+
+        all_docs = {}
+
+        # TODO: add meters
+        next_page_token = None
+        done = False
+        while not done:
+            results = self.readChunk(service, next_page_token)
+            items = results.get('files', [])
+
+            if items:
+                count += len(items)
+                for item in items:
+                    if item['trashed'] or item['mimeType'] == 'application/vnd.google-apps.document':
+                        continue
+                    logging.debug(u'{0} -> {1}'.format(item['name'], item))
+                    key = ItemKey(item)
+                    if key:
+                        if not all_docs.has_key(key):
+                            all_docs[key] = []
+                        all_docs[key] += [item]
+                        logging.debug(u'added doc id={0} {1}'.format(item['id'], item['name']))
+                    else:
+                        no_key_count += 1
+            next_page_token = results.get('nextPageToken')
+            done = not (next_page_token and count < _MAX_COUNT)
+
+        return all_docs, count, no_key_count
+
+
+    def findDups(self):
+        all_docs, count, no_key_count = self.loadDocs()
+        dups = [all_docs[key] for key in all_docs if len(all_docs[key]) > 1]
+
+        logging.info("Finished")
+        return dups, count, no_key_count
+
+
+def printDups():
+    fmt = """
+===============
+Finished. Processed files: {0}
+Found duplicate suspects: {1}
+Skipped - cannot generate key: {2}
+===============
+"""
+    lister = ListFiles(None)
+    dups, count, no_key_count = lister.findDups()
+
+    print(fmt.format(count, len(dups), no_key_count))
+    print('\n====\nDUPS: \n')
+
+    # TODO: Eliminate some false positives.
+    # TODO: print path in Drive.
+    # TODO: always write to file. Separate from log info.
+    for dup in dups:
+        print(u"found {0} duplicates for {1}".format(len(dup), dup[0]['name']))
+        for item in dup:
+            printDup(item)
 
 def printDup(item):
     path = u"/".join(item['path'])
@@ -78,55 +153,8 @@ def printDup(item):
 
 def main():
     """Reads files from Google Drive API and tries to identify duplicates."""
-    service = get_service()
-    count = 0
-    no_key_count = 0
-    all_docs = {}
-    
-    #TODO: add meters
-    results = read_chunk(service, None)
-    done = False
-    while not done:
-        items = results.get('files', [])
-        
-        if items:
-            count += len(items)
-            for item in items:
-                if item['trashed'] or item['mimeType'] == 'application/vnd.google-apps.document':
-                    continue
-                logging.debug(u'{0} -> {1}'.format(item['name'], item))
-                key = makeKey(item)
-                if key:
-                    if not all_docs.has_key(key):
-                        all_docs[key] = []
-                    all_docs[key] += [item]
-                    logging.debug(u'added doc id={0} {1}'.format(item['id'], item['name']))
-                else:
-                    no_key_count += 1
-        next_page_token = results.get('nextPageToken')
-        done = not (next_page_token and count < _MAX_COUNT)
-        results = read_chunk(service, next_page_token)
+    printDups()
 
-    dups = [all_docs[key] for key in all_docs if len(all_docs[key]) > 1]
-    logging.info("Finished")
-    print("""
-===============
-Finished. Processed files: {0}
-Found duplicate suspects: {1}
-Skipped - cannot generate key: {2}
-===============
-"""
-          .format(count, len(dups), no_key_count))
-    print('\n====\nDUPS: \n')
 
-    #TODO: Eliminate some false positives.
-    #TODO: print path in Drive.
-    #TODO: always write to file. Separate from log info.
-    for dup in dups:
-        print(u"found {0} duplicates for {1}".format(len(dup), dup[0]['name']))
-        for item in dup:
-            printDup(item)
-
- 
 if __name__ == '__main__':
     main()
---
+++ test_listitems.py
@@ -0,0 +1,92 @@
+import unittest
+from unittest import TestCase
+from listfiles import ListFiles
+from listfiles import ItemKey
+
+
+ITEM1 = {
+    u'mimeType': u'text/plain',
+    u'md5Checksum': u'a6e952b763e16a7dc6e8772e5a90f1c5',
+    u'name': u'some_file.txt',
+    u'modifiedTime': u'2017-05-22T01:50:45.610Z',
+    u'fullFileExtension': u'txt',
+    u'parents': [u'PARENT1'],
+    u'originalFilename': u'italy-movie-notes.txt',
+    u'id': u'0BylQJTWU7tFzeTlMRDIzQTdPNFE',
+    u'trashed': False,
+    u'size': u'83'
+}
+
+ITEM1A = {
+    u'mimeType': u'text/plain',
+    u'md5Checksum': u'a6e952b763e16a7dc6e8772e5a90f1c5',
+    u'name': u'some_file.txt',
+    u'modifiedTime': u'2017-05-22T01:50:45.610Z',
+    u'fullFileExtension': u'txt',
+    u'parents': [u'PARENT2'],
+    u'originalFilename': u'italy-movie-notes.txt',
+    u'id': u'0BylQJTWU7tFzeTlMRDIzQTdPNFE',
+    u'trashed': False,
+    u'size': u'83'
+}
+
+ITEM2 = {
+    u'mimeType': u'text/plain',
+    u'md5Checksum': u'a6e952b763e16a7dc6e8772e5a90f1c6',
+    u'name': u'some_file.txt',
+    u'modifiedTime': u'2017-05-22T01:50:45.610Z',
+    u'fullFileExtension': u'txt',
+    u'parents': [u'PARENT2'],
+    u'originalFilename': u'italy-movie-notes.txt',
+    u'id': u'0BylQJTWU7tFzeTlMRDIzQTdPNFE',
+    u'trashed': False,
+    u'size': u'83'
+}
+
+ITEM3 = {
+    u'mimeType': u'text/plain',
+    u'name': u'some_file.txt',
+    u'modifiedTime': u'2017-05-22T01:50:45.610Z',
+    u'fullFileExtension': u'txt',
+    u'parents': [u'PARENT3'],
+    u'originalFilename': u'italy-movie-notes.txt',
+    u'id': u'0BylQJTWU7tFzeTlMRDIzQTdPNFE',
+    u'trashed': False,
+    u'size': u'83'
+}
+
+ITEM3A = {
+    u'mimeType': u'text/plain',
+    u'name': u'some_file.txt',
+    u'modifiedTime': u'2017-05-22T01:50:45.610Z',
+    u'fullFileExtension': u'txt',
+    u'parents': [u'PARENT4'],
+    u'originalFilename': u'italy-movie-notes.txt',
+    u'id': u'0BylQJTWU7tFzeTlMRDIzQTdPNFE',
+    u'trashed': False,
+    u'size': u'83'
+}
+
+
+class Test_listfiles(TestCase):
+
+    def test_ItemKey_eq(self):
+        ki1 = ItemKey(ITEM1)
+        ki2 = ItemKey(ITEM1A)
+        self.assertEqual(ki1, ki2)
+
+    def test_ItemKey_neq(self):
+        ki1 = ItemKey(ITEM1)
+        ki2 = ItemKey(ITEM2)
+        self.assertNotEqual(ki1, ki2)
+
+    def test_ItemKey_eq_nomdsum(self):
+        ki1 = ItemKey(ITEM3)
+        ki2 = ItemKey(ITEM3A)
+        self.assertEqual(ki1, ki2)
+
+    def test_ListFiles_(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
